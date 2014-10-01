be7ec4a66de1eacfc73146f37d3bf36be5ec897d
Daniel Corin <dcorin6@gmail.com>
2014-09-30 21:11:46 -0700

    move to ajax powered selfhud

---
+++ selfhud/static/js/hud.js
@@ -0,0 +1,39 @@
+var api_array = ['github', 'goodreads', 'kippt', 'lastfm', 'strava', 'twitter', 'untappd'];
+var date_keys = ['created_at', 'date', 'start_date', 'updated_at', 'created', 'episode_watched']
+
+function api_tag(api_name) {
+	return "#" + api_name;
+}
+
+function template_id(api_name) {
+	return "#" + api_name + "_template";
+}
+
+function api_url(api_name) {
+	return '/new_hud/' + api_name;
+}
+
+function convert_date(data) {
+	for(var i = 0, l = date_keys.length; i < l; i++) {
+		if (data[date_keys[i]] !== undefined) {
+			var d = new Date(data[date_keys[i]]);
+			data[date_keys[i]] = d.toLocaleString();
+		}
+	}
+	return data;
+}
+
+function render_api(api_name) {
+	$.getJSON(api_url(api_name), function(data){
+		data.data = convert_date(data.data);
+		var template_html = $(template_id(api_name)).html();
+		var t = _.template(template_html);
+		$(api_tag(api_name)).html(t(data));
+	});
+}
+
+$(document).ready(function(){
+	for (var i = 0, l = api_array.length; i < l; i++) {
+		render_api(api_array[i]);
+	}
+});
